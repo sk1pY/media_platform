f15ff7f2cb9a7c46c23562deb80b29394e415f22
sk1pY <sk1py@bk.ru>
2025-07-24 16:32:54 +0300

    docker upd

--- setup.sh
+++ setup.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-sail=./vendor/bin/sail
 
 echo "1. Копируем .env.example -> .env"
 cp .env.example .env
 sed -i 's/^#\s*\(DB_[A-Z_]*=.*\)/\1/' .env
 
-echo "2. Устанавливаем зависимости Composer"
-docker run --rm \
-    -u "$(id -u):$(id -g)" \
-    -v "$(pwd):/var/www/html" \
-    -w /var/www/html \
-    laravelsail/php84-composer:latest \
-    composer install --ignore-platform-reqs
+echo "2. Запускаем контейнеры Sail"
+docker-compose up -d
 
-echo "3. Настраиваем Mailpit в .env"
+echo "3. Устанавливаем зависимости Composer"
+docker-compose exec composer install
+
+echo "4. Настраиваем Mailpit в .env"
 sed -i '/^MAIL_MAILER=/c\MAIL_MAILER=smtp' .env
 sed -i '/^MAIL_HOST=/c\MAIL_HOST=mailpit' .env
 sed -i '/^MAIL_PORT=/c\MAIL_PORT=1025' .env
@@ -23,33 +20,35 @@ sed -i '/^MAIL_ENCRYPTION=/c\MAIL_ENCRYPTION=null' .env
 sed -i '/^MAIL_FROM_ADDRESS=/c\MAIL_FROM_ADDRESS=demo@example.com' .env
 sed -i '/^MAIL_FROM_NAME=/c\MAIL_FROM_NAME="Demo App"' .env
 
-echo "4 Настройка БД"
+echo "5 Настройка БД"
 sed -i '/^DB_CONNECTION=/c\DB_CONNECTION=mysql' .env
 sed -i '/^DB_HOST=/c\DB_HOST=mysql' .env
 sed -i '/^DB_PORT=/c\DB_PORT=3306' .env
 sed -i '/^DB_DATABASE=/c\DB_DATABASE=media_platform' .env
 sed -i '/^DB_USERNAME=/c\DB_USERNAME=root' .env
-sed -i '/^DB_PASSWORD=/c\DB_PASSWORD=' .env
+sed -i '/^DB_PASSWORD=/c\DB_PASSWORD=root' .env
+
+echo "6. Генерируем ключ приложения"
+docker-compose artisan key:generate
+
+echo "7. миграции"
+docker-compose exec php php artisan migrate --force
+
+echo "8. Сеем "
+docker-compose exec php php artisan db:seed
+
+echo "9. Создаем символическую ссылку для storage"
+docker-compose exec php php artisan storage:link
+
+echo "10. Устанавливаем зависимости npm"
+docker-compose exec node npm install
+
+echo "11. Собираем (Vite)"
+docker-compose exec node npm run build
 
 
-echo "5. Запускаем контейнеры Sail"
-$sail up -d
 
-echo "6. Устанавливаем зависимости npm"
-$sail npm install
 
-echo "7. Генерируем ключ приложения"
-$sail artisan key:generate
 
-echo "8. Собираем (Vite)"
-$sail npm run build
 
-echo "9. миграции"
-$sail artisan migrate
-
-echo "10. Сеем "
-$sail artisan db:seed
-
-echo "11. Создаем символическую ссылку для storage"
-$sail artisan storage:link
 
